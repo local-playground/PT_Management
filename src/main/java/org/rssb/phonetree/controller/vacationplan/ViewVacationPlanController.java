bef8e54e837c7d3b6bb8598347866ec567b6d954
oxl268 <rajesh.khullar@capitalone.com>
2017-12-01 16:54:49 -0500

    Sevadars Vacation plan implementation continue

---
+++ src/main/java/org/rssb/phonetree/controller/vacationplan/ViewVacationPlanController.java
@@ -0,0 +1,10 @@
+package org.rssb.phonetree.controller.vacationplan;
+
+import org.rssb.phonetree.controller.AbstractController;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class ViewVacationPlanController extends AbstractController{
+}
